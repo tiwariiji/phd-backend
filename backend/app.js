0f5511bf980d6cfdbd91adab899b0a8884df0b3b
tiwariiji <vkt.don@gmail.com>
2025-12-07 20:35:43 +0530

    chhota change

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: ["http://localhost:5173","https://phd-addmission-frontend-rwts.vercel.app/"],
+  origin: ["http://localhost:5173","https://phd-addmission-frontend-rwts.vercel.app"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
